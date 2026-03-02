ce7e9302fd25be0df408fedddb856a492f509826
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-03-02 09:20:11 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test-cap-issue-8367-regression-android-range',
+  webDir: 'dist'
+};
+
+export default config;
